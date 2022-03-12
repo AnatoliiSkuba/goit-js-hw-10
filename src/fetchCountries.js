cd5b33d475f4b252c3be255a8bc8271792eb4650
Skuba Anatoliy <photomir.in.ua@gmail.com>
2022-03-12 19:27:53 +0200

    done

---
+++ src/fetchCountries.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+function fetchCountries(name) { 
+    const url = `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`;
+    return fetch(url).then(response => response.json());
+};
+
+export default { fetchCountries }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,57 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import API from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.com/v3.1/name/{name}').then(data => {
-    console.log(data);
-});
+const searchBox = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+countryList.style.listStyleType = "none";
+countryList.style.padding = "0";
+
+searchBox.addEventListener("input", debounce((evt) => {
+    evt.preventDefault();
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+
+    let persistedSearch = localStorage.getItem('onSearch');
+    persistedSearch = persistedSearch ? JSON.parse(persistedSearch) : "";
+    persistedSearch = evt.target.value.trim();
+    localStorage.setItem("onSearch", JSON.stringify(persistedSearch));
+    
+    if (persistedSearch !== "") {
+        API.fetchCountries(persistedSearch)
+        .then(renderCountry)
+        .catch(onFetchError)
+    };
+    
+}, DEBOUNCE_DELAY));
+
+function renderCountry(country) {
+    if (country.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (country.length <= 10 && country.length > 1) {
+        const markup = country
+        .sort((firstCountry, secondCountry) => firstCountry.name.common.localeCompare(secondCountry.name.common))
+        .map(({ flags, name }) => `<li style="display: flex; align-items: center; margin: 5px 0"><img src="${flags.svg}" width="30">&nbsp;&nbsp;${name.common}</li>`).join('');
+        countryList.insertAdjacentHTML("afterbegin", markup);
+    } else {
+        renderInfo(country[0]);
+        function renderInfo({ name, capital, population, flags, languages }) {
+            const markup = `<p style="font-weight: bold; font-size: 38px; display: flex; align-items: center;"><img src="${flags.svg}" width="30">&nbsp;${name.common}</p>
+            <p><b>Capital:</b> ${capital}</p>
+            <p><b>Population:</b> ${population}</p>
+            <p><b>Languages:</b> ${Object.values(languages)
+                .sort((firstLanguage, secondLanguage) => firstLanguage.localeCompare(secondLanguage))
+                .join(', ')}</p>`;
+            countryInfo.insertAdjacentHTML("afterbegin", markup);
+        };
+    };
+};
+
+function onFetchError(error) {
+    Notify.failure('Oops, there is no country with that name');
+    console.log(error);
+};
